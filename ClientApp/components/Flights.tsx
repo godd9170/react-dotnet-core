cba285c9eaaa9a10acbd3d18026c56bd3f6e2d04
hgoddard <hgoddard@guestlogix.com>
2017-11-05 15:06:34 -0500

    get a flights SPA page setup

---
+++ ClientApp/components/Flights.tsx
@@ -0,0 +1,24 @@
+﻿import * as React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { ApplicationState } from '../store';
+import * as FlightsStore from '../store/Flights';
+
+type FlightsProps =
+   FlightsStore.FlightsState
+    & typeof FlightsStore.actionCreators
+    & RouteComponentProps<{}>;
+
+class Flights extends React.Component<FlightsProps, {}> {
+    public render() {
+        return <div>
+            <h1>Flights Go Here.</h1>
+        </div>;
+    }
+}
+
+// Wire up the React component to the Redux store
+export default connect(
+    (state: ApplicationState) => state.flights, // Selects which state properties are merged into the component's props
+   FlightsStore.actionCreators                 // Selects which action creators are merged into the component's props
+)(Flights) as typeof Flights;
--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -4,9 +4,11 @@ import { Layout } from './components/Layout';
 import Home from './components/Home';
 import FetchData from './components/FetchData';
 import Counter from './components/Counter';
+import Flights from './components/Flights';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
     <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata/:startDateIndex?' component={ FetchData } />
+    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
+    <Route path='/flights' component={Flights} />
 </Layout>;
